9055f348600f0adab00ec459362037b673050a76
Your-Pluviophile <935664910@qq.com>
2022-08-30 21:02:43 +0800

    d2-4-5 自定义异常

--- common/service_base/src/main/java/com/atguigu/servicebase/exceptionHandler/GlobalExceptionHandler.java
+++ common/service_base/src/main/java/com/atguigu/servicebase/exceptionHandler/GlobalExceptionHandler.java
@@ -8,9 +8,17 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @ControllerAdvice
 public class GlobalExceptionHandler {
     @ResponseBody
-    @ExceptionHandler(Exception.class)
+    //指定是什么样的异常
+    @ExceptionHandler(Exception.class)//全局
     public R error(Exception e){
         e.printStackTrace();
         return R.error().message("系统繁忙，请稍后再试");
+    }
+
+    @ExceptionHandler(GuliException.class)
+    @ResponseBody
+    public R error(GuliException e){
+        e.printStackTrace();
+        return R.error().message(e.getMsg()).code(e.getCode());
     }
 }
